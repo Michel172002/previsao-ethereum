ed0c4a42c0b8076168d90d0045d7b8fb9938e1ab
Erick Michel <michel172002@gmail.com>
2023-12-30 23:47:01 -0300

    feat - tabela das apostas

---
+++ src/components/ApostasTable.js
@@ -0,0 +1,62 @@
+import Table from 'react-bootstrap/Table';
+
+const ApostasTable = () => {
+    return (
+        <Table striped bordered hover size="sm">
+            <thead>
+                <tr>
+                <th>Nome</th>
+                <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>Você</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 1</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 2</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 3</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 4</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 5</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 6</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 7</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 8</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 9</td>
+                <td>R$ 10,00</td>
+                </tr>
+                <tr>
+                <td>Oponente 10</td>
+                <td>R$ 10,00</td>
+                </tr>
+            </tbody>
+        </Table>
+    )
+}
+
+export default ApostasTable;
